5804fa5a63c7cc317fb6673d398ebda4e1d0c29d
Wanderson Chevrand <wandersonchevrand27@gmail.com>
2022-06-17 15:20:12 -0300

    Correção de erros diversos

--- atividade01-app/src/components/footer/index.jsx
+++ atividade01-app/src/components/footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <>
         </>
-    )
-};
+    );
+}
 
 export default Footer;
--- atividade01-app/src/pages/Amazonas/index.jsx
+++ atividade01-app/src/pages/Amazonas/index.jsx
@@ -1,4 +1,4 @@
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 import Header from "../../components/Header"
 
 const Amazonas = () => {
@@ -7,7 +7,7 @@ const Amazonas = () => {
             <Header />
             <Footer />
         </>
-    )
-};
+    );
+}
 
 export default Amazonas;
